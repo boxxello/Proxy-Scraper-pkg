6b8457bab5552ed18b490dce8821e0503f4fb2a4
boxxoA <francihula12@gmail.com>
2022-10-18 18:27:59 +0200

    Added args to pass in the input/output file Added arg to enable debug logging

--- proxy_scraper/__main__.py
+++ proxy_scraper/__main__.py
@@ -1,11 +1,47 @@
+import argparse
+
 from proxy_scraper.getproxy import GetProxy
+from argparse import Namespace
+
+from proxy_scraper.loggers import get_logger
+
+logger=get_logger(__name__)
+def parse_args()->Namespace:
+    parser = argparse.ArgumentParser(description='Proxy Scraper')
+    parser.add_argument(
+        "--input",
+        required=False,
+        type=str,
+        help="Input file",
+    )
+    parser.add_argument(
+        "--output",
+        required=False,
+        type=str,
+        help="Output file",
+    )
+    parser.add_argument(
+        "--debug",
+        type=bool,
+        default=True,
+        help="Enable debug logging",
+    )
+
+    args = parser.parse_args()
+    logger.info(args)
+
+    return args
 
 if __name__ == '__main__':
-    g = GetProxy()
-    g.init()
-    g.load_input_proxies()
-    g.validate_input_proxies()
-    g.load_plugins()
-    g.grab_web_proxies()
-    g.validate_web_proxies()
-    g.save_proxies()
+    args = parse_args()
+    proxy_scraper = GetProxy(
+        input_proxies_file=args.input,
+        output_proxies_file=args.output,
+    )
+    proxy_scraper.init()
+    proxy_scraper.load_input_proxies()
+    proxy_scraper.validate_input_proxies()
+    proxy_scraper.load_plugins()
+    proxy_scraper.grab_web_proxies()
+    proxy_scraper.validate_web_proxies()
+    proxy_scraper.save_proxies()
