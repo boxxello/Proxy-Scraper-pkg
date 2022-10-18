3876f2274230c0abbab875a104a00c53e155f948
boxxoA <francihula12@gmail.com>
2022-10-18 18:30:41 +0200

    Added arg to enable debug

--- proxy_scraper/__main__.py
+++ proxy_scraper/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 from proxy_scraper.getproxy import GetProxy
 from argparse import Namespace
@@ -6,6 +7,16 @@ from argparse import Namespace
 from proxy_scraper.loggers import get_logger
 
 logger=get_logger(__name__)
+def enable_debug_logging() -> None:
+    """
+    Enable debug logging for the scripts
+
+    :return: None
+    """
+    logger.setLevel(logging.DEBUG)
+    for handler in logger.handlers:
+        handler.setLevel(logging.DEBUG)
+    logger.info(f"Enabled debug logging")
 def parse_args()->Namespace:
     parser = argparse.ArgumentParser(description='Proxy Scraper')
     parser.add_argument(
@@ -34,6 +45,8 @@ def parse_args()->Namespace:
 
 if __name__ == '__main__':
     args = parse_args()
+    if args.debug:
+        enable_debug_logging()
     proxy_scraper = GetProxy(
         input_proxies_file=args.input,
         output_proxies_file=args.output,
